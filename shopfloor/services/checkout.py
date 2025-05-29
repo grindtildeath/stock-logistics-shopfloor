OCA/wms

shopfloor checkout: scan line better messages

shopfloor/services/checkout.py
@@ -471,21 +471,31 @@ class Checkout(Component):
             return self._response_for_summary(picking)
 
         # Search of the destination package
-        search_result = self._scan_line_find(picking, barcode)
-        result_handler = getattr(self, "_select_lines_from_" + search_result.type)
-        kw = {"confirm_pack_all": confirm_pack_all, "confirm_lot": confirm_lot}
-        return result_handler(picking, selection_lines, search_result.record, **kw)
+        handlers = {
+            "package": self._select_lines_from_package,
+            "product": self._select_lines_from_product,
+            "packaging": self._select_lines_from_packaging,
+            "lot": self._select_lines_from_lot,
+            "serial": self._select_lines_from_serial,
+            "delivery_packaging": self._select_lines_from_delivery_packaging,
+            "none": self._select_lines_from_none,
+        }
+        search_result = self._scan_line_find(picking, barcode, handlers.keys())
+        # setting scanned record as kwarg in order to make better logs.
+        # The reason for this is that from a product we might select various records
+        # and lose track of what was initially scanned. This forces us to display
+        # standard messages that might have no meaning for the user.
+        kwargs = {
+            "confirm_pack_all": confirm_pack_all,
+            "confirm_lot": confirm_lot,
+            "scanned_record": search_result.record,
+            "barcode": barcode,
+        }
+        handler = handlers.get(search_result.type, self._select_lines_from_none)
+        return handler(picking, selection_lines, search_result.record, **kwargs)
 
-    def _scan_line_find(self, picking, barcode, search_types=None):
+    def _scan_line_find(self, picking, barcode, search_types):
         search = self._actions_for("search")
-        search_types = (
-            "package",
-            "product",
-            "packaging",
-            "lot",
-            "serial",
-            "delivery_packaging",
-        )
         return search.find(
             barcode,
             types=search_types,
@@ -508,15 +518,14 @@ class Checkout(Component):
             lambda l: l.package_id == package and not l.shopfloor_checkout_done
         )
         if not lines:
-            return self._response_for_select_line(
-                picking,
-                message={
-                    "message_type": "error",
-                    "body": _("Package {} is not in the current transfer.").format(
-                        package.name
-                    ),
-                },
-            )
+            # No line for scanned package in selected picking
+            # Check if there's any picking reserving this product.
+            return_picking = self._get_pickings_for_package(package, limit=1)
+            if return_picking:
+                message = self.msg_store.picking_type_is_return(return_picking)
+            else:
+                message = self.msg_store.package_not_found_in_picking(package, picking)
+            return self._response_for_select_line(picking, message=message)
         self._select_lines(lines, prefill_qty=prefill_qty)
         if self.work.menu.no_prefill_qty:
             lines = picking.move_line_ids
@@ -533,9 +542,12 @@ class Checkout(Component):
 
         lines = selection_lines.filtered(lambda l: l.product_id == product)
         if not lines:
-            return self._response_for_select_line(
-                picking, message=self.msg_store.product_not_found_in_current_picking()
-            )
+            return_picking = self._get_pickings_for_product(product, limit=1)
+            if return_picking:
+                message = self.msg_store.picking_type_is_return(return_picking)
+            else:
+                message = self.msg_store.product_not_found_in_current_picking(product)
+            return self._response_for_select_line(picking, message=message)
 
         # When products are as units outside of packages, we can select them for
         # packing, but if they are in a package, we want the user to scan the packages.
@@ -1047,18 +1059,21 @@ class Checkout(Component):
             return self._response_for_select_document(message=message)
 
         selected_lines = self.env["stock.move.line"].browse(selected_line_ids).exists()
-        search_result = self._scan_package_find(picking, barcode)
-        message = self._check_scan_package_find(picking, search_result)
-        if message:
-            return self._response_for_select_package(
-                picking,
-                selected_lines,
-                message=message,
-            )
-        result_handler = getattr(
-            self, "_scan_package_action_from_" + search_result.type
-        )
-        return result_handler(picking, selected_lines, search_result.record)
+        handlers = {
+            "package": self._scan_package_action_from_package,
+            "product": self._scan_package_action_from_product,
+            "packaging": self._scan_package_action_from_packaging,
+            "lot": self._scan_package_action_from_lot,
+            "serial": self._scan_package_action_from_serial,
+            "delivery_packaging": self._scan_package_action_from_delivery_packaging,
+        }
+        search_result = self._scan_package_find(picking, barcode, handlers.keys())
+        handler = handlers.get(search_result.type, self._scan_package_action_from_none)
+        kwargs = {
+            "barcode": barcode,
+            "scanned_record": search_result.record,
+        }
+        return handler(picking, selected_lines, search_result.record, **kwargs)
 
     def _scan_package_find(self, picking, barcode, search_types=None):
         search = self._actions_for("search")
@@ -1078,10 +1093,6 @@ class Checkout(Component):
                 serial=dict(products=picking.move_lines.product_id),
             ),
         )
-
-    def _check_scan_package_find(self, picking, search_result):
-        # Used by inheriting modules
-        return False
 
     def _find_line_to_increment(self, product_lines):
         """Find which line should have its qty incremented.

shopfloor/tests/test_checkout_scan_line.py
@@ -166,6 +166,24 @@ class CheckoutScanLineCase(CheckoutScanLineCaseBase):
     def test_scan_line_error_package_not_in_picking(self):
         picking = self._create_picking(lines=[(self.product_a, 10)])
         self._fill_stock_for_moves(picking.move_lines, in_package=True)
+        picking.action_assign()
+        # Create a package for product_a
+        package = self._create_package_in_location(
+            picking.location_id, [(self.product_a, 10, None)]
+        )
+        # we work with picking, but we scan another package (not in a pick)
+        self._test_scan_line_error(
+            picking,
+            package.name,
+            {
+                "message_type": "error",
+                "body": f"Package {package.name} not found in transfer {picking.name}",
+            },
+        )
+
+    def test_scan_line_error_package_reserved_by_another_picking(self):
+        picking = self._create_picking(lines=[(self.product_a, 10)])
+        self._fill_stock_for_moves(picking.move_lines, in_package=True)
         picking2 = self._create_picking(lines=[(self.product_a, 10)])
         self._fill_stock_for_moves(picking2.move_lines, in_package=True)
         (picking | picking2).action_assign()
@@ -176,9 +194,7 @@ class CheckoutScanLineCase(CheckoutScanLineCaseBase):
             package.name,
             {
                 "message_type": "error",
-                "body": "Package {} is not in the current transfer.".format(
-                    package.name
-                ),
+                "body": f"Reserved for Checkout {picking2.name}",
             },
         )
 
@@ -247,7 +263,22 @@ class CheckoutScanLineCase(CheckoutScanLineCaseBase):
             self.product_b.barcode,
             {
                 "message_type": "error",
-                "body": "Product is not in the current transfer.",
+                "body": "Product Product B is not in the current transfer.",
+            },
+        )
+
+    def test_scan_line_error_product_in_another_picking(self):
+        picking = self._create_picking(lines=[(self.product_a, 10)])
+        self._fill_stock_for_moves(picking.move_lines, in_package=True)
+        picking2 = self._create_picking(lines=[(self.product_b, 10)])
+        self._fill_stock_for_moves(picking2.move_lines, in_package=True)
+        (picking | picking2).action_assign()
+        self._test_scan_line_error(
+            picking,
+            self.product_b.barcode,
+            {
+                "message_type": "error",
+                "body": f"Reserved for Checkout {picking2.name}",
             },
         )
 

grindtildeath/stock-logistics-shopfloor

shopfloor/services/checkout.py
@@ -473,21 +473,31 @@ class Checkout(Component):
             return self._response_for_summary(picking)
 
         # Search of the destination package
-        search_result = self._scan_line_find(picking, barcode)
-        result_handler = getattr(self, "_select_lines_from_" + search_result.type)
-        kw = {"confirm_pack_all": confirm_pack_all, "confirm_lot": confirm_lot}
-        return result_handler(picking, selection_lines, search_result.record, **kw)
+        handlers = {
+            "package": self._select_lines_from_package,
+            "product": self._select_lines_from_product,
+            "packaging": self._select_lines_from_packaging,
+            "lot": self._select_lines_from_lot,
+            "serial": self._select_lines_from_serial,
+            "delivery_packaging": self._select_lines_from_delivery_packaging,
+            "none": self._select_lines_from_none,
+        }
+        search_result = self._scan_line_find(picking, barcode, handlers.keys())
+        # setting scanned record as kwarg in order to make better logs.
+        # The reason for this is that from a product we might select various records
+        # and lose track of what was initially scanned. This forces us to display
+        # standard messages that might have no meaning for the user.
+        kwargs = {
+            "confirm_pack_all": confirm_pack_all,
+            "confirm_lot": confirm_lot,
+            "scanned_record": search_result.record,
+            "barcode": barcode,
+        }
+        handler = handlers.get(search_result.type, self._select_lines_from_none)
+        return handler(picking, selection_lines, search_result.record, **kwargs)
 
-    def _scan_line_find(self, picking, barcode, search_types=None):
+    def _scan_line_find(self, picking, barcode, search_types):
         search = self._actions_for("search")
-        search_types = (
-            "package",
-            "product",
-            "packaging",
-            "lot",
-            "serial",
-            "delivery_packaging",
-        )
         return search.find(
             barcode,
             types=search_types,
@@ -510,15 +520,14 @@ class Checkout(Component):
             lambda l: l.package_id == package and not l.shopfloor_checkout_done
         )
         if not lines:
-            return self._response_for_select_line(
-                picking,
-                message={
-                    "message_type": "error",
-                    "body": _("Package {} is not in the current transfer.").format(
-                        package.name
-                    ),
-                },
-            )
+            # No line for scanned package in selected picking
+            # Check if there's any picking reserving this product.
+            return_picking = self._get_pickings_for_package(package, limit=1)
+            if return_picking:
+                message = self.msg_store.reserved_for_other_picking_type(return_picking)
+            else:
+                message = self.msg_store.package_not_found_in_picking(package, picking)
+            return self._response_for_select_line(picking, message=message)
         self._select_lines(lines, prefill_qty=prefill_qty)
         if self.work.menu.no_prefill_qty:
             lines = picking.move_line_ids
@@ -535,9 +544,12 @@ class Checkout(Component):
 
         lines = selection_lines.filtered(lambda l: l.product_id == product)
         if not lines:
-            return self._response_for_select_line(
-                picking, message=self.msg_store.product_not_found_in_current_picking()
-            )
+            return_picking = self._get_pickings_for_product(product, limit=1)
+            if return_picking:
+                message = self.msg_store.reserved_for_other_picking_type(return_picking)
+            else:
+                message = self.msg_store.product_not_found_in_current_picking(product)
+            return self._response_for_select_line(picking, message=message)
 
         # When products are as units outside of packages, we can select them for
         # packing, but if they are in a package, we want the user to scan the packages.
@@ -1049,18 +1061,21 @@ class Checkout(Component):
             return self._response_for_select_document(message=message)
 
         selected_lines = self.env["stock.move.line"].browse(selected_line_ids).exists()
-        search_result = self._scan_package_find(picking, barcode)
-        message = self._check_scan_package_find(picking, search_result)
-        if message:
-            return self._response_for_select_package(
-                picking,
-                selected_lines,
-                message=message,
-            )
-        result_handler = getattr(
-            self, "_scan_package_action_from_" + search_result.type
-        )
-        return result_handler(picking, selected_lines, search_result.record)
+        handlers = {
+            "package": self._scan_package_action_from_package,
+            "product": self._scan_package_action_from_product,
+            "packaging": self._scan_package_action_from_packaging,
+            "lot": self._scan_package_action_from_lot,
+            "serial": self._scan_package_action_from_serial,
+            "delivery_packaging": self._scan_package_action_from_delivery_packaging,
+        }
+        search_result = self._scan_package_find(picking, barcode, handlers.keys())
+        handler = handlers.get(search_result.type, self._scan_package_action_from_none)
+        kwargs = {
+            "barcode": barcode,
+            "scanned_record": search_result.record,
+        }
+        return handler(picking, selected_lines, search_result.record, **kwargs)
 
     def _scan_package_find(self, picking, barcode, search_types):
         search = self._actions_for("search")
@@ -1080,10 +1095,6 @@ class Checkout(Component):
                 serial=dict(products=picking.move_ids.product_id),
             ),
         )
-
-    def _check_scan_package_find(self, picking, search_result):
-        # Used by inheriting modules
-        return False
 
     def _find_line_to_increment(self, product_lines):
         """Find which line should have its qty incremented.

shopfloor/tests/test_checkout_scan_line.py
@@ -166,6 +166,24 @@ class CheckoutScanLineCase(CheckoutScanLineCaseBase):
     def test_scan_line_error_package_not_in_picking(self):
         picking = self._create_picking(lines=[(self.product_a, 10)])
         self._fill_stock_for_moves(picking.move_ids, in_package=True)
+        picking.action_assign()
+        # Create a package for product_a
+        package = self._create_package_in_location(
+            picking.location_id, [(self.product_a, 10, None)]
+        )
+        # we work with picking, but we scan another package (not in a pick)
+        self._test_scan_line_error(
+            picking,
+            package.name,
+            {
+                "message_type": "error",
+                "body": f"Package {package.name} not found in transfer {picking.name}",
+            },
+        )
+
+    def test_scan_line_error_package_reserved_by_another_picking(self):
+        picking = self._create_picking(lines=[(self.product_a, 10)])
+        self._fill_stock_for_moves(picking.move_ids, in_package=True)
         picking2 = self._create_picking(lines=[(self.product_a, 10)])
         self._fill_stock_for_moves(picking2.move_ids, in_package=True)
         (picking | picking2).action_assign()
@@ -176,9 +194,7 @@ class CheckoutScanLineCase(CheckoutScanLineCaseBase):
             package.name,
             {
                 "message_type": "error",
-                "body": "Package {} is not in the current transfer.".format(
-                    package.name
-                ),
+                "body": f"Reserved for Checkout {picking2.name}",
             },
         )
 
@@ -247,7 +263,22 @@ class CheckoutScanLineCase(CheckoutScanLineCaseBase):
             self.product_b.barcode,
             {
                 "message_type": "error",
-                "body": "Product is not in the current transfer.",
+                "body": "Product Product B is not in the current transfer.",
+            },
+        )
+
+    def test_scan_line_error_product_in_another_picking(self):
+        picking = self._create_picking(lines=[(self.product_a, 10)])
+        self._fill_stock_for_moves(picking.move_ids, in_package=True)
+        picking2 = self._create_picking(lines=[(self.product_b, 10)])
+        self._fill_stock_for_moves(picking2.move_ids, in_package=True)
+        (picking | picking2).action_assign()
+        self._test_scan_line_error(
+            picking,
+            self.product_b.barcode,
+            {
+                "message_type": "error",
+                "body": f"Reserved for Checkout {picking2.name}",
             },
         )
 