acsone/wms

[16.0][MIG] - shopfloor_batch_automatic_creation

shopfloor_batch_automatic_creation/actions/picking_batch_auto_create.py
@@ -4,7 +4,7 @@
 
 import logging
 
-from odoo import fields, tools
+from odoo import Command
 
 from odoo.addons.component.core import Component
 from odoo.addons.stock.models.stock_move import PROCUREMENT_PRIORITIES
@@ -26,172 +26,34 @@ class PickingBatchAutoCreateAction(Component):
     def create_batch(
         self,
         picking_types,
-        max_pickings=0,
-        max_weight=0,
-        max_volume=0,
         group_by_commercial_partner=False,
+        maximum_number_of_preparation_lines=False,
+        stock_device_types=None,
     ):
-        self._lock()
-        search_user = self.env.user
-        pickings = self._search_pickings(picking_types, user=search_user)
-        if not pickings:
-            search_user = None
-            pickings = self._search_pickings(picking_types)
-        pickings = self._sort(pickings)
-        if group_by_commercial_partner:
-            # From the first operation we got, get all operations having the
-            # same commercial entity
-            picking = fields.first(pickings)
-            commercial_partner = picking.partner_id.commercial_partner_id
-            pickings = self._search_pickings(
-                picking_types, user=search_user, commercial_partner=commercial_partner
+        make_picking_batch = self.env["make.picking.batch"].create(
+            self._prepare_make_picking_batch_values(
+                picking_types,
+                group_by_commercial_partner=group_by_commercial_partner,
+                maximum_number_of_preparation_lines=maximum_number_of_preparation_lines,
+                stock_device_types=stock_device_types,
             )
-        else:
-            # Otherwise process by priorities by applying the limits
-            pickings = self._apply_limits(
-                pickings, max_pickings, max_weight, max_volume
-            )
-        if not pickings:
-            return self.env["stock.picking.batch"].browse()
-        return self._create_batch(pickings)
-
-    def _lock(self):
-        """Lock to prevent concurrent creation of batch
-
-        Use a blocking advisory lock to prevent 2 transactions to create
-        a batch at the same time. The lock is released at the commit or
-        rollback of the transaction.
-
-        The creation of a new batch should be short enough not to block
-        the users for too long.
-        """
-        _logger.info(
-            "trying to acquire lock to create a picking batch (%s)", self.env.user.login
         )
-        self._actions_for("lock").advisory(self._advisory_lock_name)
-        # very much probably outdated already (i.e. if the transaction which
-        # had the lock before this one set a 'batch_id' on stock.picking this
-        # transaction will not be aware of), we'll probably have a retry. But
-        # the lock can help limit the number of retries.
-        _logger.info(
-            "lock acquired to create a picking batch (%s)", self.env.user.login
-        )
+        return make_picking_batch._create_batch(raise_if_not_possible=False)
 
-    def _search_pickings_domain(
-        self, picking_types, user=None, commercial_partner=None
+    def _prepare_make_picking_batch_values(
+        self,
+        picking_types,
+        group_by_commercial_partner=False,
+        maximum_number_of_preparation_lines=False,
+        stock_device_types=None,
     ):
-        domain = [
-            ("picking_type_id", "in", picking_types.ids),
-            ("state", "in", ("assigned", "partially_available")),
-            ("batch_id", "=", False),
-            ("user_id", "=", user.id if user else False),
-        ]
-        if commercial_partner:
-            domain.append(
-                ("partner_id.commercial_partner_id", "=", commercial_partner.id)
-            )
-        return domain
+        values = {
+            "group_pickings_by_partner": group_by_commercial_partner,
+            "maximum_number_of_preparation_lines": maximum_number_of_preparation_lines,
+        }
+        if picking_types and picking_types.ids:
+            values["picking_type_ids"] = [Command.set(picking_types.ids)]
 
-    def _search_pickings(self, picking_types, user=None, commercial_partner=None):
-        # We can't use a limit in the SQL search because the 'priority' fields
-        # is sometimes empty (it seems the inverse StockPicking.priority field
-        # mess up with default on stock.move), we have to sort in Python.
-        return self.env["stock.picking"].search(
-            self._search_pickings_domain(
-                picking_types, user=user, commercial_partner=commercial_partner
-            )
-        )
-
-    def _sort(self, pickings):
-        return pickings.sorted(
-            lambda picking: (
-                -(int(picking.priority) if picking.priority else PRIORITY_NORMAL),
-                picking.scheduled_date,
-                picking.id,
-            )
-        )
-
-    def _precision_weight(self):
-        return self.env["decimal.precision"].precision_get("Product Unit of Measure")
-
-    def _precision_volume(self):
-        return max(
-            6,
-            self.env["decimal.precision"].precision_get("Product Unit of Measure") * 2,
-        )
-
-    def _apply_limits(self, pickings, max_pickings, max_weight, max_volume):
-        first_picking = fields.first(pickings)
-        current_priority = first_picking.priority or PRIORITY_NORMAL
-        # Always take the first picking even if it doesn't fullfill the
-        # weight/volume criteria. This allows to always process at least
-        # one picking which won't be processed otherwise
-        selected_pickings = first_picking
-
-        precision_weight = self._precision_weight()
-        precision_volume = self._precision_volume()
-
-        def gt(value1, value2, digits):
-            """Return True if value1 is greater than value2"""
-            return tools.float_compare(value1, value2, precision_digits=digits) == 1
-
-        total_weight = self._picking_weight(first_picking)
-        total_volume = self._picking_volume(first_picking)
-        for picking in pickings[1:]:
-            if max_pickings and len(selected_pickings) == max_pickings:
-                # selected enough!
-                break
-            if (picking.priority or PRIORITY_NORMAL) != current_priority:
-                # as we sort by priority, exit as soon as the priority changes,
-                # we do not mix priorities to make delivery of high priority
-                # transfers faster
-                break
-
-            weight = self._picking_weight(picking)
-            volume = self._picking_volume(picking)
-            if max_weight and gt(total_weight + weight, max_weight, precision_weight):
-                continue
-
-            if max_volume and gt(total_volume + volume, max_volume, precision_volume):
-                continue
-
-            selected_pickings |= picking
-            total_weight += weight
-            total_volume += volume
-
-        return selected_pickings
-
-    def _picking_weight(self, picking):
-        weight = 0.0
-        for line in picking.move_lines:
-            weight += line.product_id.get_total_weight_from_packaging(
-                line.product_uom_qty
-            )
-        return weight
-
-    def _picking_volume(self, picking):
-        volume = 0.0
-        for line in picking.move_lines:
-            product = line.product_id
-            packagings_with_volume = product.with_context(
-                _packaging_filter=lambda p: p.volume
-            ).product_qty_by_packaging(line.product_uom_qty)
-            for packaging_info in packagings_with_volume:
-                if packaging_info.get("is_unit"):
-                    pack_volume = product.volume
-                else:
-                    packaging = self.env["product.packaging"].browse(
-                        packaging_info["id"]
-                    )
-                    pack_volume = packaging.volume
-
-                volume += pack_volume * packaging_info["qty"]
-        return volume
-
-    def _create_batch(self, pickings):
-        return self.env["stock.picking.batch"].create(
-            self._create_batch_values(pickings)
-        )
-
-    def _create_batch_values(self, pickings):
-        return {"picking_ids": [(6, 0, pickings.ids)]}
+        if stock_device_types and stock_device_types.ids:
+            values["stock_device_type_ids"] = [Command.set(stock_device_types.ids)]
+        return values

grindtildeath/stock-logistics-shopfloor

shopfloor_batch_automatic_creation/actions/picking_batch_auto_create.py
@@ -4,7 +4,7 @@
 
 import logging
 
-from odoo import fields, tools
+from odoo import Command
 
 from odoo.addons.component.core import Component
 from odoo.addons.stock.models.stock_move import PROCUREMENT_PRIORITIES
@@ -26,172 +26,38 @@ class PickingBatchAutoCreateAction(Component):
     def create_batch(
         self,
         picking_types,
-        max_pickings=0,
-        max_weight=0,
-        max_volume=0,
         group_by_commercial_partner=False,
+        maximum_number_of_preparation_lines=False,
+        stock_device_types=None,
+        **kwargs
     ):
-        self._lock()
-        search_user = self.env.user
-        pickings = self._search_pickings(picking_types, user=search_user)
-        if not pickings:
-            search_user = None
-            pickings = self._search_pickings(picking_types)
-        pickings = self._sort(pickings)
-        if group_by_commercial_partner:
-            # From the first operation we got, get all operations having the
-            # same commercial entity
-            picking = fields.first(pickings)
-            commercial_partner = picking.partner_id.commercial_partner_id
-            pickings = self._search_pickings(
-                picking_types, user=search_user, commercial_partner=commercial_partner
+        make_picking_batch = self.env["make.picking.batch"].create(
+            self._prepare_make_picking_batch_values(
+                picking_types,
+                group_by_commercial_partner=group_by_commercial_partner,
+                maximum_number_of_preparation_lines=maximum_number_of_preparation_lines,
+                stock_device_types=stock_device_types,
+                **kwargs
             )
-        else:
-            # Otherwise process by priorities by applying the limits
-            pickings = self._apply_limits(
-                pickings, max_pickings, max_weight, max_volume
-            )
-        if not pickings:
-            return self.env["stock.picking.batch"].browse()
-        return self._create_batch(pickings)
-
-    def _lock(self):
-        """Lock to prevent concurrent creation of batch
-
-        Use a blocking advisory lock to prevent 2 transactions to create
-        a batch at the same time. The lock is released at the commit or
-        rollback of the transaction.
-
-        The creation of a new batch should be short enough not to block
-        the users for too long.
-        """
-        _logger.info(
-            "trying to acquire lock to create a picking batch (%s)", self.env.user.login
         )
-        self._actions_for("lock").advisory(self._advisory_lock_name)
-        # very much probably outdated already (i.e. if the transaction which
-        # had the lock before this one set a 'batch_id' on stock.picking this
-        # transaction will not be aware of), we'll probably have a retry. But
-        # the lock can help limit the number of retries.
-        _logger.info(
-            "lock acquired to create a picking batch (%s)", self.env.user.login
-        )
+        return make_picking_batch._create_batch(raise_if_not_possible=False)
 
-    def _search_pickings_domain(
-        self, picking_types, user=None, commercial_partner=None
+    def _prepare_make_picking_batch_values(
+        self,
+        picking_types,
+        group_by_commercial_partner=False,
+        maximum_number_of_preparation_lines=False,
+        stock_device_types=None,
+        **kwargs
     ):
-        domain = [
-            ("picking_type_id", "in", picking_types.ids),
-            ("state", "in", ("assigned", "partially_available")),
-            ("batch_id", "=", False),
-            ("user_id", "=", user.id if user else False),
-        ]
-        if commercial_partner:
-            domain.append(
-                ("partner_id.commercial_partner_id", "=", commercial_partner.id)
-            )
-        return domain
+        values = {
+            "restrict_to_same_partner": group_by_commercial_partner,
+            "maximum_number_of_preparation_lines": maximum_number_of_preparation_lines,
+            "restrict_to_same_priority": True,
+        }
+        if picking_types and picking_types.ids:
+            values["picking_type_ids"] = [Command.set(picking_types.ids)]
 
-    def _search_pickings(self, picking_types, user=None, commercial_partner=None):
-        # We can't use a limit in the SQL search because the 'priority' fields
-        # is sometimes empty (it seems the inverse StockPicking.priority field
-        # mess up with default on stock.move), we have to sort in Python.
-        return self.env["stock.picking"].search(
-            self._search_pickings_domain(
-                picking_types, user=user, commercial_partner=commercial_partner
-            )
-        )
-
-    def _sort(self, pickings):
-        return pickings.sorted(
-            lambda picking: (
-                -(int(picking.priority) if picking.priority else PRIORITY_NORMAL),
-                picking.scheduled_date,
-                picking.id,
-            )
-        )
-
-    def _precision_weight(self):
-        return self.env["decimal.precision"].precision_get("Product Unit of Measure")
-
-    def _precision_volume(self):
-        return max(
-            6,
-            self.env["decimal.precision"].precision_get("Product Unit of Measure") * 2,
-        )
-
-    def _apply_limits(self, pickings, max_pickings, max_weight, max_volume):
-        first_picking = fields.first(pickings)
-        current_priority = first_picking.priority or PRIORITY_NORMAL
-        # Always take the first picking even if it doesn't fullfill the
-        # weight/volume criteria. This allows to always process at least
-        # one picking which won't be processed otherwise
-        selected_pickings = first_picking
-
-        precision_weight = self._precision_weight()
-        precision_volume = self._precision_volume()
-
-        def gt(value1, value2, digits):
-            """Return True if value1 is greater than value2"""
-            return tools.float_compare(value1, value2, precision_digits=digits) == 1
-
-        total_weight = self._picking_weight(first_picking)
-        total_volume = self._picking_volume(first_picking)
-        for picking in pickings[1:]:
-            if max_pickings and len(selected_pickings) == max_pickings:
-                # selected enough!
-                break
-            if (picking.priority or PRIORITY_NORMAL) != current_priority:
-                # as we sort by priority, exit as soon as the priority changes,
-                # we do not mix priorities to make delivery of high priority
-                # transfers faster
-                break
-
-            weight = self._picking_weight(picking)
-            volume = self._picking_volume(picking)
-            if max_weight and gt(total_weight + weight, max_weight, precision_weight):
-                continue
-
-            if max_volume and gt(total_volume + volume, max_volume, precision_volume):
-                continue
-
-            selected_pickings |= picking
-            total_weight += weight
-            total_volume += volume
-
-        return selected_pickings
-
-    def _picking_weight(self, picking):
-        weight = 0.0
-        for line in picking.move_lines:
-            weight += line.product_id.get_total_weight_from_packaging(
-                line.product_uom_qty
-            )
-        return weight
-
-    def _picking_volume(self, picking):
-        volume = 0.0
-        for line in picking.move_lines:
-            product = line.product_id
-            packagings_with_volume = product.with_context(
-                _packaging_filter=lambda p: p.volume
-            ).product_qty_by_packaging(line.product_uom_qty)
-            for packaging_info in packagings_with_volume:
-                if packaging_info.get("is_unit"):
-                    pack_volume = product.volume
-                else:
-                    packaging = self.env["product.packaging"].browse(
-                        packaging_info["id"]
-                    )
-                    pack_volume = packaging.volume
-
-                volume += pack_volume * packaging_info["qty"]
-        return volume
-
-    def _create_batch(self, pickings):
-        return self.env["stock.picking.batch"].create(
-            self._create_batch_values(pickings)
-        )
-
-    def _create_batch_values(self, pickings):
-        return {"picking_ids": [(6, 0, pickings.ids)]}
+        if stock_device_types and stock_device_types.ids:
+            values["stock_device_type_ids"] = [Command.set(stock_device_types.ids)]
+        return values