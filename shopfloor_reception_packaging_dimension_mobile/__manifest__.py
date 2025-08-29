2bbf0683ffd35c7b5405ec65c58b84951026f312
Simone Orsi <simahawk@gmail.com>
2025-08-29 15:15:51 +0200

    shopfloor_reception_packaging_dimension_mobile: migrate to v18

--- shopfloor_reception_packaging_dimension_mobile/__manifest__.py
+++ shopfloor_reception_packaging_dimension_mobile/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Shopfloor Reception Packaging Dimension Mobile",
     "summary": "Frontend for the packaging dimension on reception scenario",
-    "version": "14.0.1.1.0",
+    "version": "18.0.1.0.0",
     "development_status": "Alpha",
     "category": "Inventory",
     "website": "https://github.com/OCA/stock-logistics-shopfloor",
