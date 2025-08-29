cf7b6694982bef0b639ea329104b8b4116b6fa8b
Simone Orsi <simahawk@gmail.com>
2025-08-29 15:14:47 +0200

    [IMP] shopfloor_reception_packaging_dimension_mobile: pre-commit auto fixes

--- shopfloor_reception_packaging_dimension_mobile/__manifest__.py
+++ shopfloor_reception_packaging_dimension_mobile/__manifest__.py
@@ -7,7 +7,7 @@
     "version": "14.0.1.1.0",
     "development_status": "Alpha",
     "category": "Inventory",
-    "website": "https://github.com/OCA/wms",
+    "website": "https://github.com/OCA/stock-logistics-shopfloor",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainers": ["TDu"],
     "license": "AGPL-3",
