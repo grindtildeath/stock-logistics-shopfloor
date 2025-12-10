071ad88c1e0c91762b28cc5c6d82399ce4082b23
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2025-12-10 12:18:40 +0100

    fixup! Add shopfloor_reception_measuring_device

--- shopfloor_reception_measuring_device/tests/device_model.py
+++ shopfloor_reception_measuring_device/tests/device_model.py
@@ -1,10 +1,11 @@
 # Copyright 2025 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+# pylint: disable=consider-merging-classes-inherited
 
 from odoo import fields, models
 
 
-class MeasuringModel(models.Model):  # pylint: disable=consider-merging-classes-inherited
+class MeasuringModel(models.Model):
     _inherit = "measuring.device"
 
     device_type = fields.Selection(selection_add=[("testdevice", "TestDevice")])
